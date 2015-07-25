2ec264ed371bce4db1d73e44e4962a95e4ad0c58
Anarchy <anarchy@anarchyY5070>
2015-07-25 17:46:24 +0200

    Initial commit for blogger

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+QuickBlog::Application.config.secret_token = '6b0fc98d92875209f4c7d77bef8b2d947809e9b4a902c7b82d8155b0e98ca4a5f1e069318816554c9a7571fb29fb67143856d2c510cd17e388fd906b8dcb85ca'
